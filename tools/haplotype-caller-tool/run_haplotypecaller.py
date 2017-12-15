fb1c87319564dd55af7deb13e2144df2cee2fb2d
Allison Creason <alliecreason@gmail.com>
2017-12-14 22:07:17 -0800

    add haplotype caller tool

---
+++ tools/haplotype-caller-tool/run_haplotypecaller.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+import argparse
+import os
+import subprocess
+
+def build_cmd():
+    cmd = ["gatk-launch", "HaplotypeCaller", "--input", args.input, "--reference", args.reference,
+            "--output", os.join.path(args.work,args.output)]
+
+    if args.emitRefConfidence:
+        cmd.append("--emitRefConfidence")
+
+    return cmd
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    args = parser.add_argument("--input", required=True, help="BAM/SAM/CRAM file containing reads")
+    args = parser.add_argument("--reference", required=True, help="Reference sequence file")
+    args = parser.add_argument("--emitRefConfidence", required=False)
+    args = parser.add_argument("--output", required=True, help="File to which variants should be written")
+    args = parser.add_argument("--workDir", default="/work")
+    args = parser.parse_args()
+
+    # Run birdsuite
+    cmd = build_cmd()
+    print "Running Haplotype Caller\n%s" % (" ".join(cmd))
+    proc = subprocess.Popen(cmd)
+    proc.wait()
